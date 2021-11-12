dfc23d8e3c848d13b9a0092b112f829c401255c8
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-11-12 23:40:56 +0530

    Value, Object, PageStorage Keys added

--- lib/ui/news/news_page.dart
+++ lib/ui/news/news_page.dart
@@ -1,37 +1,3 @@
-/*
- * Copyright (c) 2021 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for
- * pedagogical or instructional purposes related to programming, coding,
- * application development, or information technology.  Permission for such
- * use, copying, modification, merger, publication, distribution, sublicensing,
- * creation of derivative works, or sale is expressly withheld.
- *
- * This project and source code may use libraries or frameworks that are
- * released under various Open-Source licenses. Use of those libraries and
- * frameworks are governed by their own individual licenses.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 import 'package:flutter/material.dart';
 
 import '../../data/repository/news_repository.dart';
@@ -54,6 +20,25 @@ class _NewsPageState extends State<NewsPage> {
   bool isLoading = true;
 
   //TODO: Preserve News State on tab change
+// 1
+  void updateNewsState() {
+    // 2
+    final fetchedNews =
+        PageStorage.of(context)!.readState(context, identifier: widget.key);
+
+    // 3
+    if (fetchedNews != null) {
+      setNewsState(fetchedNews);
+    } else {
+      fetchNews();
+    }
+  }
+
+// 4
+  void saveToPageStorage(List<NewsModel> newNewsState) {
+    PageStorage.of(context)!
+        .writeState(context, newNewsState, identifier: widget.key);
+  }
 
   void setNewsState(
     List<NewsModel> newNewsState, {
@@ -72,6 +57,7 @@ class _NewsPageState extends State<NewsPage> {
       final fetchedNews = await widget.newsRepository.fetchTopNews();
       final shuffledNews = ListUtils.shuffle(fetchedNews) as List<NewsModel>;
       setNewsState(shuffledNews);
+      saveToPageStorage(shuffledNews);
     } on Exception catch (_) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
@@ -84,23 +70,38 @@ class _NewsPageState extends State<NewsPage> {
   @override
   void initState() {
     super.initState();
-    fetchNews();
+    updateNewsState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.secondaryColor,
-      body: isLoading ? const ProgressWidget() : buildNewsList(),
-    );
+    return isLoading
+        ? const ProgressWidget()
+        :
+        // 1
+        RefreshIndicator(
+            child: buildNewsList(),
+            // 2
+            onRefresh: fetchNews,
+            color: AppColors.primary,
+          );
   }
 
   ListView buildNewsList() {
     return ListView(
       children: news
           .map(
-            (newsItem) => NewsItemWidget(
-              newsItem: newsItem,
+            (newsItem) => Column(
+              key: ValueKey<int>(newsItem.id),
+              children: [
+                NewsItemWidget(
+                  newsItem: newsItem,
+                ),
+                const Divider(
+                  color: Colors.black54,
+                  height: 0,
+                ),
+              ],
             ),
           )
           .toList(),
